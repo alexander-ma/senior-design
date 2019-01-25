4760cda61be40373d4aa04ef169212d9220a0857
Christopher Gill <christophergill@wireless-10-145-63-98.public.utexas.edu>
2019-01-25 14:40:55 -0600

    added comments for file for humans

--- facebook/send-message.sh
+++ facebook/send-message.sh
@@ -3,8 +3,8 @@
 ../platform-tools/adb.exe shell am start -n com.facebook.orca/com.facebook.orca.auth.StartScreenActivity
 sleep 2
 
-echo tap first Messenger contact
-../platform-tools/adb.exe shell input tap 289 690
+echo tap first Messenger contact # printing message to console for debugging
+../platform-tools/adb.exe shell input tap 289 690 # tapping on coordinates 289x690 on phone
 
 echo tap message line
 ../platform-tools/adb.exe shell input tap 556 1748
@@ -12,7 +12,8 @@ echo tap message line
 array=("testing" "hmm" "bless" "Naeem")
 echo ${#array[@]}
 
-for element in ${array[@]}
+# typing and sending each word on messenger
+for element in ${array[@]} 
 do
     echo Send message $element
     ../platform-tools/adb.exe shell input text $element
