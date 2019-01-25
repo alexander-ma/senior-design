6f548e1976d6ff0cc180395080f625ffa3dbfb29
Kevin <kevinchau@utexas.edu>
2019-01-25 16:16:31 -0600

    Working on combining tcpdump and automation scripts

---
+++ messenger/send-message.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+../adb.exe shell am start -n com.facebook.orca/com.facebook.orca.auth.StartScreenActivity
+sleep 2
+
+echo tap first Messenger contact # printing message to console for debugging
+../adb.exe shell input tap 289 690 # tapping on coordinates 289x690 on phone
+
+echo tap message line
+../adb.exe shell input tap 556 1748
+
+array=("testing" "hmm" "bless" "Naeem")
+echo ${#array[@]}
+
+# typing and sending each word on messenger
+for element in ${array[@]} 
+do
+    echo Send message $element
+    ../adb.exe shell input text $element
+    echo tap send
+    ../adb.exe shell input tap 1004 962
+
+done
+
+echo "Finished"
--- youtube/play-video.sh
+++ youtube/play-video.sh
@@ -1,3 +1,4 @@
-platform-tools/adb.exe shell am start -a android.intent.action.VIEW "http://www.youtube.com/watch?v=4EGc3r3qni8"
-sleep 10s
-platform-tools/adb.exe shell am force-stop com.google.android.youtube
+#!/bin/bash
+../adb.exe shell am start -a android.intent.action.VIEW "http://www.youtube.com/watch?v=4EGc3r3qni8"
+sleep 10
+../adb.exe shell am force-stop com.google.android.youtube
