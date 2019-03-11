b6d1216038f34b4ab5f5fe6487404664471b05d9
Jayaj Poudel <jej@wireless-10-145-97-179.public.utexas.edu>
2019-03-11 14:54:02 -0500

    updated scripts

--- instagram/IgSearchBrowse.sh
+++ instagram/IgSearchBrowse.sh
@@ -1,31 +1,52 @@
 #!/bin/bash
 
-adb shell am start -n com.instagram.android/com.instagram.android.activity.MainTabActivity
-sleep 2
+while getopts t:s: option; do
+    case $option in
+        t) TIME=$OPTARG;;
+        s) PHONE_ID=$OPTARG;;
+    esac
+done
 
-adb shell input tap 347 1824
+script_name=`basename "$0"`
+script_name=${script_name::-3}
+dir_name=`dirname "$0"`
+dir_name=${dir_name:2}
+
+echo 'STARTING TCPDUMP...'
+adb -s $PHONE_ID shell tcpdump -i any -s 0 -w "/sdcard/${dir_name}_${script_name}__${TIME}_${PHONE_ID}.pcap" &
+PID=$!
+adb -s $PHONE_ID shell am start -n com.instagram.android/com.instagram.android.activity.MainTabActivity
 sleep 2
-adb shell input tap 411 174
+#open search bar in instagram and search
+adb $PHONE_ID shell input tap 347 1824
+sleep 2
+adb $PHONE_ID shell input tap 411 174
 sleep 1
-adb shell input tap 411 174
+adb $PHONE_ID shell input tap 411 174
 sleep 2
 searchTerm="@dog.lovers"
-adb shell input text $searchTerm
-adb shell input tap 751 486
+adb $PHONE_ID shell input text $searchTerm
+adb $PHONE_ID shell input tap 751 486
 sleep 1
+
 for i in {1..5}
 do
 	sleep 2
-	adb shell input swipe 500 1500 500 100 1000
+	adb $PHONE_ID shell input swipe 500 1500 500 100 1000 #swipe
 	sleep 1
-	adb shell input tap 460 794
+	adb $PHONE_ID shell input tap 460 794 #Open picture
 	sleep 2
 
-	adb shell input tap 91 73
+	adb $PHONE_ID shell input tap 91 73 #close picture
 done
 
 
 echo ${#array[@]}
+
+kill ${PID}
+sleep 3
+adb -s $PHONE_ID pull "/sdcard/${dir_name}_${script_name}__${TIME}_${PHONE_ID}.pcap" "pcap/${dir_name}_${script_name}__${TIME}_${PHONE_ID}.pcap" 
+adb -s $PHONE_ID shell rm "/sdcard/${dir_name}_${script_name}__${TIME}_${PHONE_ID}.pcap"
 
 
 echo "Finished"
--- youtube/catSearch.sh
+++ youtube/catSearch.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
-adb shell am start -n com.google.android.youtube/com.google.android.apps.youtube.app.WatchWhileActivity
+
+while getopts t:s: option; do
+    case $option in
+        t) TIME=$OPTARG;;
+        s) PHONE_ID=$OPTARG;;
+    esac
+done
+
+script_name=`basename "$0"`
+script_name=${script_name::-3}
+dir_name=`dirname "$0"`
+dir_name=${dir_name:2}
+
+echo 'STARTING TCPDUMP...'
+adb -s $PHONE_ID shell tcpdump -i any -s 0 -w "/sdcard/${dir_name}_${script_name}__${TIME}_${PHONE_ID}.pcap" &
+PID=$!
+adb -s $PHONE_ID shell am start -n com.google.android.youtube/com.google.android.apps.youtube.app.WatchWhileActivity
 
 sleep 2
-adb shell input tap 838 125
+adb -s $PHONE_ID shell input tap 838 125
 sleep 1
-searchTerm="funny"
+searchTerm="cats"
 
 echo $searchTerm
-adb shell input text $searchTerm
+adb -s $PHONE_ID shell input text $searchTerm
 sleep 2
 
-adb shell input tap 958 1846
+adb -s $PHONE_ID shell input tap 958 1846
 
-adb shell /data/local/tmp/mysendevent /dev/input/event2 /sdcard/youtube_scroll.txt
+for i in {1..5}
+do
+	adb $PHONE_ID shell input swipe 500 1500 500 100 1000 #swipe
+	sleep 2
+done
+
+kill ${PID}
+sleep 3
+adb -s $PHONE_ID pull "/sdcard/${dir_name}_${script_name}__${TIME}_${PHONE_ID}.pcap" "pcap/${dir_name}_${script_name}__${TIME}_${PHONE_ID}.pcap" 
+adb -s $PHONE_ID shell rm "/sdcard/${dir_name}_${script_name}__${TIME}_${PHONE_ID}.pcap"
+
+
+echo "Finished"
