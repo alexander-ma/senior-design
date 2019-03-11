c816bb85b62ce1050ed9eb930de00a73c8729a5e
chriscuit <chrisraygill@utexas.edu>
2019-03-11 15:15:55 -0500

    Fix scripts missing 'shell' word

--- google-maps/download-map.sh
+++ google-maps/download-map.sh
@@ -13,32 +13,32 @@ dir_name=`dirname "$0"`
 dir_name=${dir_name:2}
 
 echo 'STARTING TCPDUMP...'
-adb -s $PHONE_ID shell tcpdump -i any -s 0 -w "/sdcard/${dir_name}_${script_name}__${TIME}_${SERIAL_ID}.pcap" &
+adb -s $PHONE_ID shell tcpdump -i any -s 0 -w "/sdcard/${dir_name}_${script_name}__${TIME}_${PHONE_ID}.pcap" &
 PID=$!
-adb -s $PHONE_ID am start -a android.intent.action.VIEW com.google.android.apps.maps/com.google.android.maps.MapsActivity
+adb -s $PHONE_ID shell am start -a android.intent.action.VIEW com.google.android.apps.maps/com.google.android.maps.MapsActivity
 sleep 10
 
 # Center on current location
-adb -s $PHONE_ID input tap 930 1250
+adb -s $PHONE_ID shell input tap 930 1250
 sleep 2
 
 # Search for location
 location="Seattle"
-adb -s $PHONE_ID input tap 563 169
+adb -s $PHONE_ID shell input tap 563 169
 sleep 1
-adb -s $PHONE_ID input text $location
-adb -s $PHONE_ID input keyevent 66
+adb -s $PHONE_ID shell input text $location
+adb -s $PHONE_ID shell input keyevent 66
 sleep 5
 
 # Download offline map from hamburger icon
-adb -s $PHONE_ID input tap 110 183
-adb -s $PHONE_ID input tap 463 1670
-adb -s $PHONE_ID input tap 580 360
-adb -s $PHONE_ID input tap 800 1850
+adb -s $PHONE_ID shell input tap 110 183
+adb -s $PHONE_ID shell input tap 463 1670
+adb -s $PHONE_ID shell input tap 580 360
+adb -s $PHONE_ID shell input tap 800 1850
 sleep 20
 
 
-adb -s $PHONE_ID am force-stop com.google.android.apps.maps
+adb -s $PHONE_ID shell am force-stop com.google.android.apps.maps
 echo 'STOPPING TCPDUMP...'
 kill ${PID}
 sleep 3
--- google-maps/explore.sh
+++ google-maps/explore.sh
@@ -8,37 +8,37 @@ while getopts t:s: option; do
 done
 
 echo 'STARTING TCPDUMP...'
-adb -s $PHONE_ID shell tcpdump -i any -s 0 -w "/sdcard/${dir_name}_${script_name}__${TIME}_${SERIAL_ID}.pcap" &
+adb -s $PHONE_ID shell tcpdump -i any -s 0 -w "/sdcard/${dir_name}_${script_name}__${TIME}_${PHONE_ID}.pcap" &
 PID=$!
-adb -s $PHONE_ID am start -a android.intent.action.VIEW com.google.android.apps.maps/com.google.android.maps.MapsActivity
+adb -s $PHONE_ID shell am start -a android.intent.action.VIEW com.google.android.apps.maps/com.google.android.maps.MapsActivity
 sleep 10
 
 # Center on current location
-adb -s $PHONE_ID input tap 930 1250
+adb -s $PHONE_ID shell input tap 930 1250
 sleep 2
 
 # Search for location
 location="Seattle"
-adb -s $PHONE_ID input tap 563 169
+adb -s $PHONE_ID shell input tap 563 169
 sleep 1
-adb -s $PHONE_ID input text $location
-adb -s $PHONE_ID input keyevent 66
+adb -s $PHONE_ID shell input text $location
+adb -s $PHONE_ID shell input keyevent 66
 sleep 5
-adb -s $PHONE_ID input keyevent 4
+adb -s $PHONE_ID shell input keyevent 4
 
 # Explore Nearby, choose one of Restaurants, Coffee, or Events
 possible_x=(158 408 670) 
 selected_x=${possible_x[$RANDOM % ${#possible_x[@]} ]}
-adb -s $PHONE_ID input tap 551 1690
-adb -s $PHONE_ID input tap $selected_x 1428
+adb -s $PHONE_ID shell input tap 551 1690
+adb -s $PHONE_ID shell input tap $selected_x 1428
 sleep 5
 for i in {1..5}
 do
 	sleep 1
-	adb -s $PHONE_ID input swipe 500 1500 500 100 1000
+	adb -s $PHONE_ID shell input swipe 500 1500 500 100 1000
 done
 
-adb -s $PHONE_ID am force-stop com.google.android.apps.maps
+adb -s $PHONE_ID shell am force-stop com.google.android.apps.maps
 echo 'STOPPING TCPDUMP...'
 kill ${PID}
 sleep 3
