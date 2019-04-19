a82065366e5cdcbf2622c7affbffb43971c2ab2a
Chriscuit <chrisraygill@utexas.edu>
2019-04-19 16:07:18 -0500

    created a bunch of manual collection scripts

---
+++ instagram/man-IgSearchBrowse.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+while getopts t:s: option; do
+    case $option in
+        t) TIME=$OPTARG;;
+        s) PHONE_ID=$OPTARG;;
+    esac
+done
+
+script_name=`basename "$0"`
+script_name=${script_name%???}
+dir_name=`dirname "$0"`
+dir_name=${dir_name:2}
+
+echo 'STARTING TCPDUMP...'
+adb -s $PHONE_ID shell tcpdump -i any -s 0 -w "/sdcard/${dir_name}_${script_name}_${TIME}_${PHONE_ID}.pcap" &
+PID=$!
+
+adb -s $PHONE_ID shell am start -n com.instagram.android/com.instagram.android.activity.MainTabActivity
+sleep 5
+
+echo 'RECORDING'
+
+#manual navigation
+sleep 150
+
+echo 'STOPPING TCPDUMP...'
+kill ${PID}
+sleep 3
+adb -s $PHONE_ID shell am force-stop com.instagram.android
+sleep 3
+echo 'Generating .pcap file...'
+adb -s $PHONE_ID pull "/sdcard/${dir_name}_${script_name}_${TIME}_${PHONE_ID}.pcap" "pcap/${dir_name}_${script_name}_${TIME}_${PHONE_ID}.pcap" 
+adb -s $PHONE_ID shell rm "/sdcard/${dir_name}_${script_name}_${TIME}_${PHONE_ID}.pcap"
