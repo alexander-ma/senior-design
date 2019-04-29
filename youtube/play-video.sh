859278e30cc20698aa482ccbfaa404b191566df5
Neil Charles <neilabhishek96@gmail.com>
2019-04-29 15:15:39 -0500

    updated youtube

--- youtube/play-video.sh
+++ youtube/play-video.sh
@@ -15,7 +15,17 @@ dir_name=${dir_name:2}
 echo 'STARTING TCPDUMP...'
 adb -s $PHONE_ID shell tcpdump -i any -s 0 -w "/sdcard/${dir_name}_${script_name}_${TIME}_${PHONE_ID}.pcap" &
 PID=$!
-adb -s $PHONE_ID shell am start -a android.intent.action.VIEW "http://www.youtube.com/watch?v=4EGc3r3qni8"
+
+num=$((1 + RANDOM % 3))
+if (( $num == 1 )); then
+    link="http://www.youtube.com/watch?v=4EGc3r3qni8"
+elif (( $num == 2 )); then
+    link="https://www.youtube.com/watch?v=qC-U5gAbMHs"
+else
+    link="https://www.youtube.com/watch?v=BT4RlDl7z3w"
+fi
+
+adb -s $PHONE_ID shell am start -a android.intent.action.VIEW link
 sleep 10
 
 adb -s $PHONE_ID shell am force-stop com.google.android.youtube
